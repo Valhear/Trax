cc1e18412618cef5a0819078ae88e269e0b63435
Valentina Henao <valen410@gmail.com>
2016-10-20 14:33:31 -0500

    trax

---
+++ Trax/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Trax
+//
+//  Created by Valentina Henao on 12/10/15.
+//  Copyright © 2015 Valentina Henao. All rights reserved.
+//
+
+import UIKit
+
+struct GPXURL {
+    static let Notification = "GPXURL Radio Station"
+    static let Key = "GPXURL URL Key"
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        print("url = \(url)")
+        let center = NSNotificationCenter.defaultCenter()
+            let notification = NSNotification(name: GPXURL.Notification, object: self, userInfo: [GPXURL.Key:url])
+        center.postNotification(notification)
+        return true
+    }
+
+}
+
